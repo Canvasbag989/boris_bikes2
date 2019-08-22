038dbf3c1dc0f5798b28ab4500cfd60323adc5e2
Mike <mjwalshe@gmail.com>
2019-08-22 15:50:21 +0100

    Add test for error raising

--- spec/docking_station_spec.rb
+++ spec/docking_station_spec.rb
@@ -3,11 +3,16 @@ require 'bike'
 
 describe DockingStation do
   it { is_expected.to respond_to :release_bike }
-  it {is_expected.to respond_to(:bike)}
-  it 'releases working bikes' do
-    bike = subject.release_bike
-    expect(bike).to be_working
-  end
+  it { is_expected.to respond_to(:bike) }
+  
+  # it 'releases working bikes' do
+  #   bike = subject.release_bike
+  #   expect(bike).to be_working
+  # end
+
+  it 'raises error when no bikes are available' do
+    expect { subject.release_bike }.to raise_error
+  end 
 
   it 'docks something' do
     bike = Bike.new
