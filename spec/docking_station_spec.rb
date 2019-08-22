fa0436f2b2b867e762db4c641d54a4b6237deb85
Mike <mjwalshe@gmail.com>
2019-08-22 17:41:50 +0100

    Add guard conditions

--- spec/docking_station_spec.rb
+++ spec/docking_station_spec.rb
@@ -3,7 +3,7 @@ require 'bike'
 
 describe DockingStation do
   it { is_expected.to respond_to :release_bike }
-  it { is_expected.to respond_to(:bike) }
+  it { is_expected.to respond_to(:bikes) }
   it { is_expected.to respond_to(:dock).with(1).argument }
 
   it 'releases working bikes' do
@@ -16,12 +16,17 @@ describe DockingStation do
     expect { subject.release_bike }.to raise_error
   end
 
-  it 'docks something' do
-    bike = Bike.new
-    expect(subject.dock(bike)).to eq bike
+  describe '#dock' do
+    it 'docks something' do
+      bike = Bike.new
+      expect(subject.dock(bike)).to eq "Bike docked"
+    end
   end
+
+describe '#dock' do
   it 'raises an error when full' do
-    subject.dock(Bike.new)
-    expect { subject.dock Bike.new }.to raise_error 'Docking station full'
+      20.times { subject.dock Bike.new }
+      expect { subject.dock Bike.new }.to raise_error 'Docking station full'
+    end
   end
 end
