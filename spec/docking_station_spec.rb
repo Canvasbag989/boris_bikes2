aa2812bc0e16d34f8b1875a3f8ecb4df606e7874
Canvasbag989 <Canvasbag989@icloud.com>
2019-08-22 15:06:03 +0100

    Changed file names & updated docking station method

--- spec/docking_station_spec.rb
+++ spec/docking_station_spec.rb
@@ -1,21 +1,16 @@
-require 'borisbikes'
+require 'docking_station'
+require 'bike'
 
 describe DockingStation do
   it { is_expected.to respond_to :release_bike }
-
+  it {is_expected.to respond_to(:bike)}
   it 'releases working bikes' do
     bike = subject.release_bike
     expect(bike).to be_working
   end
-end
 
-describe DockingStation do
   it 'docks something' do
     bike = Bike.new
     expect(subject.dock(bike)).to eq bike
-end
-end
-
-describe DockingStation do
-  it {is_expected.to respond_to(:bike)}
+  end
 end
